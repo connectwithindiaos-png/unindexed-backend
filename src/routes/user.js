51401f25c8ec1c40b362d7a06fe669cddaaf22e7
Amit Rajwar <amitxrajwar@gmail.com>
2026-06-16 11:39:06 -0700

    Add user icon upload endpoints (POST/GET/DELETE /api/user/icon)

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,25 +1,42 @@
 const { Router } = require('express');
 const apkController = require('../controllers/apkController');
+const iconController = require('../controllers/iconController');
 const { authenticate } = require('../middleware/auth');
 
 const router = Router();
 
 router.use(authenticate);
 
-router.get('/apk', (req, res, next) => {
+function requireUser(req, res, next) {
   if (!req.user) {
     return res.status(403).json({ error: 'Token user access required' });
   }
+  next();
+}
+
+router.get('/apk', requireUser, (req, res, next) => {
   req.params.id = req.user.tokenId;
   apkController.generate(req, res, next);
 });
 
-router.get('/apk/logs', (req, res, next) => {
-  if (!req.user) {
-    return res.status(403).json({ error: 'Token user access required' });
-  }
+router.get('/apk/logs', requireUser, (req, res, next) => {
   req.params.id = req.user.tokenId;
   apkController.streamLogs(req, res, next);
+});
+
+router.post('/icon', requireUser, (req, res, next) => {
+  req.params.id = req.user.tokenId;
+  iconController.upload(req, res, next);
+});
+
+router.get('/icon', requireUser, (req, res, next) => {
+  req.params.id = req.user.tokenId;
+  iconController.get(req, res, next);
+});
+
+router.delete('/icon', requireUser, (req, res, next) => {
+  req.params.id = req.user.tokenId;
+  iconController.delete(req, res, next);
 });
 
 module.exports = router;
